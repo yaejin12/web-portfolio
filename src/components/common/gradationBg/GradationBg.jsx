9c0f76a36919500c85c285df381437026c7e8525
yaejin12 <pos03167@naver.com>
2024-09-30 02:09:55 +0900

    css 수정

--- src/components/common/gradationBg/GradationBg.jsx
+++ src/components/common/gradationBg/GradationBg.jsx
@@ -1,15 +1,21 @@
 import React, { Children, useEffect, useState } from "react";
 import styles from "./GradationBg.module.scss";
+import { useLocation, useParams } from "react-router-dom";
 
 function GradationBg({ children }) {
   const [xy, setXY] = useState({ x: 760, y: 160 });
   const [isMouseMove, setIsMouseMove] = useState(false);
+  const location = useLocation();
+  const { id } = useParams();
 
   // ======마우스 효과=======
+  // || isDetailPage
   const handleMouseMove = (e) => {
     if (!isMouseMove) return;
-    setXY({ x: e.clientX - 250, y: e.clientY - 200 });
+    setXY({ x: e.clientX - 200, y: e.clientY - 200 });
   };
+
+  const isDetailPage = location.pathname.startsWith("/project/") && id;
 
   //2초 뒤 마우스 이벤트 활성화
   useEffect(() => {
--- src/pages/layouts/components/MenuNav.jsx
+++ src/pages/layouts/components/MenuNav.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation, useParams } from "react-router-dom";
 import backImg from "../../../assets/images/icons/back.svg";
 
 function MenuNav({ styles }) {
   const location = useLocation();
+  const { id } = useParams();
   const isIntroduce = location.pathname === "/introduce";
   const isProject = location.pathname === "/project";
 
@@ -11,7 +12,7 @@ function MenuNav({ styles }) {
     if (isIntroduce) {
       return styles.isIntroduce;
     }
-    if (isProject) {
+    if (isProject || location.pathname.includes("/project")) {
       return styles.isProject;
     } else {
       return "";
@@ -22,13 +23,6 @@ function MenuNav({ styles }) {
     <nav className={styles.menu}>
       <div className={styles.inner}>
         <ul className={`${styles.gnb} ${ClassFn()}`}>
-          {/* <li className={styles.backImgWrapper}>
-            {isIntroduce && (
-              <NavLink to="/">
-                <img src={backImg}></img>
-              </NavLink>
-            )}
-          </li> */}
           <li className={styles.nav}>
             <NavLink to="/introduce">
               <div className={styles.intro}>INTRODUCE</div>
