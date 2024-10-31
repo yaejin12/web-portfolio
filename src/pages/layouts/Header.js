e05acda359e66411f4afc341995ea819fbe17476
yaejin12 <pos03167@naver.com>
2024-11-01 02:34:43 +0900

    header 수정

--- src/pages/layouts/Header.js
+++ src/pages/layouts/Header.js
@@ -6,12 +6,11 @@ function Header({ styles }) {
   // Redux에서 isBlackRef 상태를 가져옴
   const isRef = useSelector((state) => state.isBlackRef.isBlackRef);
 
-  
   return (
     <header>
-      <div className={styles.headerWrapper}>
-        <MenuNav styles={styles} isRef={isRef} />
-      </div>
+      {/* <div className={styles.headerWrapper}> */}
+      <MenuNav styles={styles} isRef={isRef} />
+      {/* </div> */}
     </header>
   );
 }
